67e3549d45ef67081aa4760454cb945fb6111da6
technovision99 <25871300+technovision99@users.noreply.github.com>
2022-11-10 08:40:34 -0800

    added hardhat config

--- part4/contracts/crytic/EchidnaUniV2Tester.sol
+++ part4/contracts/crytic/EchidnaUniV2Tester.sol
@@ -1,4 +1,4 @@
-pragma solidity 0.5.16;
+pragma solidity >=0.5.16;
 import "./Setup.sol";
 
 contract EchidnaUniV2Tester is Setup {
@@ -10,12 +10,12 @@ contract EchidnaUniV2Tester is Setup {
        //PRECONDITIONS:
         uint kBefore = testPair.kLast(); 
         (uint reserveBefore1, uint reserveBefore2) = UniswapV2Library.getReserves(address(factory), address(testToken1), address(testToken2));
-        uint productBefore = reserveBefore1 * reserveBefore2;
+        uint productBefore = reserveBefore1 * reserveBefore2; 
        //CALL:
         (bool success, bytes memory ret) = users[1].proxy(address(router), abi.encodeWithSelector(router.swapExactTokensForTokens.selector, swapAmountIn,0,[address(testToken1),address(testToken2)],users[1],block.timestamp));
         //POSTCONDITIONS:
         if(success) {
-            uint[] memory amounts = abi.decode(ret,(uint[] memory));
+            uint[] memory amounts = abi.decode(ret,(uint[]));
             uint kAfter = testPair.kLast(); 
             (uint reserveAfter1, uint reserveAfter2) = UniswapV2Library.getReserves(address(factory), address(testToken1), address(testToken2));
             uint productAfter = reserveAfter1*reserveAfter2;

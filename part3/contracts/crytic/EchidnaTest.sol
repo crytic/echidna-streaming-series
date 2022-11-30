2b6fdbfa16d9427798cc17c6d6577423cfbaef31
technovision99 <25871300+technovision99@users.noreply.github.com>
2022-11-30 11:03:45 -0800

    updated with part3

---
+++ part3/contracts/crytic/EchidnaTest.sol
@@ -0,0 +1,38 @@
+pragma solidity ^0.6.0;
+
+import "./Setup.sol";
+
+contract EchidnaTest is Setup {
+    function testProvideLiquidity(uint amount1, uint amount2) public {
+        //Preconditions: 
+        amount1 = _between(amount1, 1000, uint(-1));
+        amount2 = _between(amount2, 1000, uint(-1));
+
+        if(!completed) {
+            _mintTokens(amount1, amount2);
+        }
+        uint lpTokenBalanceBefore = pair.balanceOf(address(user));
+        (uint reserve0Before, uint reserve1Before,) = pair.getReserves();
+        uint kBefore = reserve0Before * reserve1Before;
+
+        (bool success1,) = user.proxy(address(testToken1),abi.encodeWithSelector(testToken1.transfer.selector, address(pair),amount1));
+        (bool success2,) = user.proxy(address(testToken2),abi.encodeWithSelector(testToken2.transfer.selector, address(pair),amount2));
+        require(success1 && success2);
+
+        //Action:
+        (bool success3,) = user.proxy(address(pair),abi.encodeWithSelector(bytes4(keccak256("mint(address)")), address(user)));
+        
+        //Postconditions:
+        if(success3) {
+            uint lpTokenBalanceAfter = pair.balanceOf(address(user));
+            (uint reserve0After, uint reserve1After,) = pair.getReserves();
+            uint kAfter = reserve0After * reserve1After;
+            assert(lpTokenBalanceBefore < lpTokenBalanceAfter);
+            assert(kBefore < kAfter);
+
+        }
+
+
+    }
+
+}

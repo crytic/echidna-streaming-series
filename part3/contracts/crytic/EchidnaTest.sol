582ea13a02135455b3be35932624f0daf0af4e0e
Ardis Lu <ardisglu@gmail.com>
2022-11-30 23:13:32 -0800

    Add testInitialLiquidity

--- part3/contracts/crytic/EchidnaTest.sol
+++ part3/contracts/crytic/EchidnaTest.sol
@@ -35,4 +35,24 @@ contract EchidnaTest is Setup {
 
     }
 
+    function testInitialLiquidity(uint amount1, uint amount2) public {
+        // Precondition: user attempts to create a new pair without supplying minimum liquidity
+        require(pair.totalSupply() == 0);
+        amount1 = _between(amount1, 1, 1000);
+        amount2 = _between(amount2, 1, 1000);
+
+        if(!completed) {
+            _mintTokens(amount1, amount2);
+        }
+        (bool success1, ) = user.proxy(address(testToken1), abi.encodeWithSelector(testToken1.transfer.selector, address(pair), amount1));
+        (bool success2, ) = user.proxy(address(testToken2), abi.encodeWithSelector(testToken2.transfer.selector, address(pair), amount2));
+        require(success1 && success2);
+
+        // Action: attempt to get LP tokens
+        (bool success3, ) = user.proxy(address(pair), abi.encodeWithSelector(bytes4(keccak256("mint(address)")), address(user)));
+
+        // Postconditions:
+        assert(!success3); // Since minimum liquidity was not provided, mint should revert with 'UniswapV2: INSUFFICIENT_LIQUIDITY_MINTED'
+        assert(pair.balanceOf(address(user)) == 0); // User should not be awarded any LP tokens
+    }
 }

e9b6fbf4960bc4d44bc589ea1f01e66b34fc8e5f
Natalie Chin <natalie.chin@trailofbits.com>
2022-12-14 09:54:19 -0500

    Fixed bug in allocate/remove to save pre-reserves

--- part5/contracts/echidna/LibraryMathEchidna.sol
+++ part5/contracts/echidna/LibraryMathEchidna.sol
@@ -1,14 +1,7 @@
 pragma solidity 0.8.6;
-import "../libraries/Margin.sol";
 import "../libraries/Reserve.sol";
-import "../libraries/Units.sol";
 
 contract LibraryMathEchidna { 
-	using Units for uint256;
-	using Units for int128;
-	mapping (address => Margin.Data) private margins;
-	Margin.Data margin;
-
 	event LogUint256(string msg, uint256 value);
 	event AssertionFailed(string msg, uint256 expected, uint256 value);
 
@@ -26,23 +19,21 @@ contract LibraryMathEchidna {
 		if (!isSetup) { 
 			setupReserve(); // set up the reserve with a starting value because delta liquidity value relies on non-zero reserves
 		}
-		Reserve.Data memory preAllocateReserves; //save the pre-allocation reserve balances
+		Reserve.Data memory preAllocateReserves = reserve; //save the pre-allocation reserve balances
 	
 		//************************* Action *************************/
 		uint256 liquidity0 = (delRisky * reserve.liquidity) / uint256(reserve.reserveRisky); // calculate the spot price on the risky token
         uint256 liquidity1 = (delStable * reserve.liquidity) / uint256(reserve.reserveStable); // calculate the spot price on the stable token
-        uint256 delLiquidity = liquidity0 < liquidity1 ? liquidity0 : liquidity1; //choose min(risky spot, stable spot) for liquidity change
+        delLiquidity = liquidity0 < liquidity1 ? liquidity0 : liquidity1; //choose min(risky spot, stable spot) for liquidity change
 		
 		Reserve.allocate(reserve,delRisky,delStable,delLiquidity,uint32(block.timestamp + 1000)); // allocate to the reserve
 
 		//************************* Post-Conditions *************************/
-		/* Asserts: 
-		  - pre-allocation risky balance + risky amount allocated = post-allocation risky balance
-		  - pre-allocation stable balance + stable amount allocated = post-allocation stable balance 
-		  - pre-liquidity allocation + delta liquidity = post-allocation liquidity
-		*/
+		// pre-allocation risky balance + risky amount allocated = post-allocation risky balance
 		assert(preAllocateReserves.reserveRisky + delRisky == reserve.reserveRisky);
+		// pre-allocation stable balance + stable amount allocated = post-allocation stable balance
 		assert(preAllocateReserves.reserveStable + delStable == reserve.reserveStable);
+		// pre-liquidity allocation + delta liquidity = post-allocation liquidity
 		assert(preAllocateReserves.liquidity + delLiquidity == reserve.liquidity);
 
 		return (preAllocateReserves, delLiquidity);
@@ -53,7 +44,7 @@ contract LibraryMathEchidna {
 			setupReserve(); // set up the reserve with starting value if it has not been started before
 		}		
 		// delLiquidity = _between(delLiquidity, 1, type(uint64).max);		
-		Reserve.Data memory preRemoveReserves; // save the pre-remove reserve balances
+		Reserve.Data memory preRemoveReserves = reserve; // save the pre-remove reserve balances
 		
 		//************************* Action *************************/
 		(uint256 delRisky, uint256 delStable) = Reserve.getAmounts(reserve,delLiquidity); // calculate the amount of risky and stable tokens the incoming liquidity maps to 
@@ -61,100 +52,44 @@ contract LibraryMathEchidna {
 		Reserve.remove(reserve,delRisky,delStable,delLiquidity,uint32(block.timestamp + 1000)); // call Reserve.remove
 
 		//************************* Post-Conditions *************************/
-		/** Asserts: 
-			- pre-remove risky balance = current reserve.reserveRisky + risky amount removed
-			- pre-remove stable balance = current reserve.reserveRisky + risky amount stable
-			- pre-remove liquidity = current reserve.liquidity + deltaLiquidity
-		 */
+		// pre-remove risky balance = current reserve.reserveRisky + risky amount removed
 		assert(preRemoveReserves.reserveRisky == reserve.reserveRisky + delRisky);
+		// pre-remove stable balance = current reserve.reserveRisky + risky amount stable
 		assert(preRemoveReserves.reserveStable == reserve.reserveStable + delStable );
+		// pre-remove liquidity = current reserve.liquidity + deltaLiquidity
 		assert(preRemoveReserves.liquidity == reserve.liquidity + delLiquidity);
 	}
 
 	function allocate_then_remove(uint256 delRisky, uint256 delStable) public {
 		//************************* Pre-Conditions *************************/
+		// bound the incoming inputs 
 		delRisky = _between(delRisky, 1, type(uint64).max);
 		delStable = _between(delStable, 1, type(uint64).max);
 
+		// if an assertion fails, print out the delRisky and delStable values (as the actual args may not be equivalent to the incoming args)
 		emit LogUint256("delRisky", delRisky);
 		emit LogUint256("delStable", delStable);
 		
 		//************************* Action *************************/
-		/**
-			- call reserve allocate, saving pre-allocat reserves and associated delLiquidity value 
-			- call reserve remove with delLiquidity from allocate
-		 */
-		(Reserve.Data memory preAllocateReserves,
-		uint256 delAllocateLiquidity
-		) = reserve_allocate(delRisky, delStable);
-
+		// call reserve allocate, saving pre-allocate reserves and associated delLiquidity value 
+		(Reserve.Data memory preAllocateReserves, uint256 delAllocateLiquidity) = reserve_allocate(delRisky, delStable);
+		// call reserve remove with delLiquidity from allocate
 		reserve_remove(delAllocateLiquidity);
 
 		//************************* Post-Conditions *************************/
-		/*
-			- pre-allocate reserveRisky = current (post-remove) reserveRisky
-			- pre-allocate reserveStable = current (post-remove) reserveStable 
-			- pre-allocate liquidity = current (post-remove) liquidity
-		*/
-		bool shouldFail;
+		// pre-allocate reserveRisky = current (post-remove) reserveRisky
 		if(preAllocateReserves.reserveRisky != reserve.reserveRisky) { 
 			emit AssertionFailed("preallocate risky not equivalent to current risky", preAllocateReserves.reserveRisky, reserve.reserveRisky);
-			shouldFail = true;
 		}
+		// pre-allocate reserveStable = current (post-remove) reserveStable 
 		if(preAllocateReserves.reserveStable  != reserve.reserveStable) {
 			emit AssertionFailed("preallocate stable not equivalent to current stable", preAllocateReserves.reserveStable, reserve.reserveStable);
-			shouldFail = true;		
 		}
+		// pre-allocate liquidity = current (post-remove) liquidity
 		if(preAllocateReserves.liquidity  != reserve.liquidity) {
 			emit AssertionFailed("preallocate liq not equivalent to current liq", preAllocateReserves.liquidity, reserve.liquidity);
-			shouldFail = true;					
-		}
-		if (shouldFail) {
-			assert(false);
 		}
 	}
-
-	// --------------------- Units.sol -----------------------
-	function scaleUpAndScaleDownInverses(uint256 value, uint256 factor) public {
-		uint256 scaledFactor = (10e18+ factor % (10e18 + 1));
-
-		uint256 scaledUpValue = value.scaleUp(scaledFactor);
-		uint256 scaledDownValue = scaledUpValue.scaleDown(scaledFactor);
-		
-		assert(scaledDownValue == value);
-	}
-	function scaleToAndFromX64Inverses(uint256 value, uint256 _decimals) public {
-		// will enforce factor between 0 - 12
-		uint256 factor = _decimals % (13); 
-		// will enforce scaledFactor between 1 - 10**12 , because 10**0 = 1
-		uint256 scaledFactor = 10**factor;
-
-		int128 scaledUpValue = value.scaleToX64(scaledFactor);
-		uint256 scaledDownValue = scaledUpValue.scaleFromX64(scaledFactor);
-		
-		assert(scaledDownValue == value);
-	}
-
-	// --------------------- Margins.sol -----------------------
-	function margin_deposit(uint256 riskyAmt, uint256 stableAmt) public {
-		uint256 preRisky = margin.balanceRisky;
-		uint256 preStable = margin.balanceStable;
-
-		Margin.deposit(margin, riskyAmt, stableAmt);
-		
-		assert(margin.balanceRisky - riskyAmt == preRisky);
-		assert(margin.balanceStable - stableAmt == preStable);
-	}
-	function margin_withdraw(uint256 riskyAmt, uint256 stableAmt) public {
-		uint256 preRisky = margin.balanceRisky;
-		uint256 preStable = margin.balanceStable;
-
-		Margin.withdraw(margins, riskyAmt, stableAmt);
-		
-		assert(margin.balanceRisky == preRisky - riskyAmt);
-		assert(margin.balanceStable == preStable - stableAmt);
-	}
-
 	function _between(uint256 random, uint256 low, uint256 high) private returns (uint256) {
 		return low + random % (high-low);
 	}

7eebfcd2cfe24686505ffae99babc41aed5415bd
webthethird <ratzbodell@gmail.com>
2022-12-30 13:22:55 -0600

    Begin rewriting Part 3 as I follow along with the tutorial video

--- part3/contracts/crytic/Setup.sol
+++ part3/contracts/crytic/Setup.sol
@@ -11,33 +11,28 @@ contract Users {
 }
 
 contract Setup {
-    UniswapV2Factory factory;
-    UniswapV2Pair pair;
     UniswapV2ERC20 testToken1;
     UniswapV2ERC20 testToken2;
+    UniswapV2Factory factory;
+    UniswapV2Pair pair;
     Users user;
     bool completed;
-    
+
     constructor() public {
         testToken1 = new UniswapV2ERC20();
         testToken2 = new UniswapV2ERC20();
         factory = new UniswapV2Factory(address(this));
-        address testPair = factory.createPair(address(testToken1), address(testToken2));
-        pair = UniswapV2Pair(testPair);
+        pair = UniswapV2Pair(factory.createPair(address(testToken1), address(testToken2)));
         user = new Users();
-        user.proxy(address(testToken1),abi.encodeWithSelector(testToken1.approve.selector, address(pair),uint(-1)));
-        user.proxy(address(testToken2), abi.encodeWithSelector(testToken2.approve.selector,address(pair),uint(-1)));
     }
 
-    function _init(uint amount1, uint amount2) internal {
+    function _mintTokens(uint amount1, uint amount2) internal {
         testToken1.mint(address(user), amount1);
         testToken2.mint(address(user), amount2);
         completed = true;
     }
 
-    function _between(uint val, uint low, uint high) internal pure returns(uint) {
-        return low + (val % (high-low +1)); 
+    function _between(uint value, uint low, uint high) internal pure returns (uint) {
+        return (low + (value % (high - low + 1)));
     }
-
-
 }

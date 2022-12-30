6f1111a26912dc91d2e28f3c447e064e82c6fb5b
webthethird <ratzbodell@gmail.com>
2022-12-30 14:10:50 -0600

    Rewrite EchidnaTest.sol

--- part3/contracts/crytic/EchidnaTest.sol
+++ part3/contracts/crytic/EchidnaTest.sol
@@ -3,53 +3,33 @@ pragma solidity ^0.6.0;
 import "./Setup.sol";
 
 contract EchidnaTest is Setup {
-    event logUints(uint unit1, uint unit2);
     function testProvideLiquidity(uint amount1, uint amount2) public {
-        //Preconditions: 
-        amount1 = _between(amount1, 1000, uint(-1));
-        amount2 = _between(amount2, 1000, uint(-1));
+        // Pre-conditions:
+        amount1 = _between(amount1, pair.MINIMUM_LIQUIDITY, uint(-1));
+        amount2 = _between(amount2, pair.MINIMUM_LIQUIDITY, uint(-1));
 
-        if(!completed) {
-            _init(amount1, amount2);
+        if (!completed) {
+            _mintTokens(amount1, amount2);
         }
+        //// State before
         uint lpTokenBalanceBefore = pair.balanceOf(address(user));
-        (uint reserve0Before, uint reserve1Before,) = pair.getReserves();
+        (uint reserve0Before, uint reserve1Before) = pair.getReserves();
         uint kBefore = reserve0Before * reserve1Before;
-
-        (bool success1,) = user.proxy(address(testToken1),abi.encodeWithSelector(testToken1.transfer.selector, address(pair),amount1));
-        (bool success2,) = user.proxy(address(testToken2),abi.encodeWithSelector(testToken2.transfer.selector, address(pair),amount2));
+        //// Transfer tokens to UniswapV2Pair contract
+        (bool success1,) = user.proxy(address(testToken1), abi.encodeWithSelector(testToken1.transfer.selector, address(pair), amount1));
+        (bool success2,) = user.proxy(address(testToken2), abi.encodeWithSelector(testToken2.transfer.selector, address(pair), amount2));
         require(success1 && success2);
 
-        //Action:
-        (bool success3,) = user.proxy(address(pair),abi.encodeWithSelector(bytes4(keccak256("mint(address)")), address(user)));
-        
-        //Postconditions:
-        if(success3) {
+        // Action:
+        (bool success3,) = user.proxy(address(pair), abi.encodeWithSelector(bytes4(keccak256("mint(address)")), address(user)));
+
+        // Post-conditions:
+        if (success3) {
             uint lpTokenBalanceAfter = pair.balanceOf(address(user));
-            (uint reserve0After, uint reserve1After,) = pair.getReserves();
+            (uint reserve0After, uint reserve1After) = pair.getReserves();
             uint kAfter = reserve0After * reserve1After;
             assert(lpTokenBalanceBefore < lpTokenBalanceAfter);
             assert(kBefore < kAfter);
-
         }
-
-    }
-    function testSwap(uint amount1, uint amount2) public {
-        
-        if(!completed) {
-            _init(amount1, amount2);
-        }
-        
-        //Preconditions
-        pair.sync(); // we matched the balances with reserves
-        require(pair.balanceOf(address(user)) > 0); //there is liquidity for the swap
-        //Call:
-        (bool success1,) = user.proxy(address(pair), abi.encodeWithSelector(pair.swap.selector, amount1,amount2,address(user),""));
-
-        //Postcondition:
-        assert(!success1); //call should never succeed
-
-
-    
     }
 }

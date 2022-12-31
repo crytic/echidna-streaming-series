8b3b4626821a951367a9f80e723d7d8bf0242d90
webthethird <ratzbodell@gmail.com>
2022-12-31 15:50:20 -0600

    Fill out `testSwap`

--- part3/contracts/crytic/EchidnaTest.sol
+++ part3/contracts/crytic/EchidnaTest.sol
@@ -1,24 +1,31 @@
 pragma solidity ^0.6.0;
 
 import "./Setup.sol";
+import "../libraries/UniswapV2Library.sol";
 
 contract EchidnaTest is Setup {
+    event AmountsIn(uint amount0, uint amount1);
+    event AmountsOut(uint amount0, uint amount1);
+    event BalancesBefore(uint balance0, uint balance1);
+    event BalancesAfter(uint balance0, uint balance1);
+    event ReservesBefore(uint reserve0, uint reserve1);
+    event ReservesAfter(uint reserve0, uint reserve1);
 
-    function testProvideLiquidity(uint amount1, uint amount2) public {
+    function testProvideLiquidity(uint amount0, uint amount1) public {
         // Pre-conditions:
+        amount0 = _between(amount0, 1000, uint(-1));
         amount1 = _between(amount1, 1000, uint(-1));
-        amount2 = _between(amount2, 1000, uint(-1));
 
         if (!completed) {
-            _init(amount1, amount2);
+            _init(amount0, amount1);
         }
         //// State before
         uint lpTokenBalanceBefore = pair.balanceOf(address(user));
         (uint reserve0Before, uint reserve1Before,) = pair.getReserves();
         uint kBefore = reserve0Before * reserve1Before;
         //// Transfer tokens to UniswapV2Pair contract
-        (bool success1,) = user.proxy(address(testToken1), abi.encodeWithSelector(testToken1.transfer.selector, address(pair), amount1));
-        (bool success2,) = user.proxy(address(testToken2), abi.encodeWithSelector(testToken2.transfer.selector, address(pair), amount2));
+        (bool success1,) = user.proxy(address(testToken1), abi.encodeWithSelector(testToken1.transfer.selector, address(pair), amount0));
+        (bool success2,) = user.proxy(address(testToken2), abi.encodeWithSelector(testToken2.transfer.selector, address(pair), amount1));
         require(success1 && success2);
 
         // Action:
@@ -34,9 +41,9 @@ contract EchidnaTest is Setup {
         }
     }
 
-    function testBadSwap(uint amount1, uint amount2) public {
+    function testBadSwap(uint amount0, uint amount1) public {
         if (!completed) {
-            _init(amount1, amount2);
+            _init(amount0, amount1);
         }
 
         // Pre-conditions:
@@ -44,19 +51,96 @@ contract EchidnaTest is Setup {
         require(pair.balanceOf(address(user)) > 0);
 
         // Action:
-        (bool success,) = user.proxy(address(pair), abi.encodeWithSelector(pair.swap.selector, amount1, amount2, address(user), ""));
+        (bool success,) = user.proxy(address(pair), abi.encodeWithSelector(pair.swap.selector, amount0, amount1, address(user), ""));
 
         // Post-condition:
         assert(!success);
     }
     
-    function testSwap(uint amount1, uint amount2) public {
+    function testSwap(uint amount0, uint amount1) public {
         // Pre-conditions:
+        if (!completed) {
+            _init(amount0, amount1);
+        }
+        require(pair.balanceOf(address(user)) > 0);
+        require(amount0 > 0 && amount1 > 0);
+
+        uint balance0Before = testToken1.balanceOf(address(user));
+        uint balance1Before = testToken2.balanceOf(address(user));
+
+        (uint reserve0Before, uint reserve1Before,) = pair.getReserves();
+        uint kBefore = reserve0Before * reserve1Before;
+        emit ReservesBefore(reserve0Before, reserve1Before);
+        
+        uint amount0In = _between(amount0, 1, reserve0Before - 1);
+        uint amount1In = _between(amount1, 1, reserve1Before - 1);
+        // emit AmountsIn(amount0In, amount1In);
+        if (amount0In > balance0Before) {
+            testToken1.mint(address(user), amount0In - balance0Before);
+            balance0Before = testToken1.balanceOf(address(user));
+        }
+        if (amount1In > balance1Before) {
+            testToken2.mint(address(user), amount1In - balance1Before);
+            balance1Before = testToken2.balanceOf(address(user));
+        }
+        require(amount0In <= balance0Before || amount1In <= balance1Before);
+        emit BalancesBefore(balance0Before, balance1Before);
+        
+        uint amount0Out;
+        uint amount1Out;
+
+        /**
+         * Precondition of UniswapV2Pair.swap is that we transfer the token we are swapping in first.
+         * So, we pick the larger of the two input amounts to transfer, and also use
+         * the Uniswap library to determine how much of the other we will receive in return.
+         */
+        if (amount0In > balance0Before) {
+            amount0In = 0;
+        } else if (amount1In > balance1Before) {
+            amount1In = 0;
+        }
+        if (amount0In > amount1In) {
+            require(amount0In <= balance0Before);
+            amount1In = 0;
+            amount0Out = 0;
+            amount1Out = UniswapV2Library.getAmountOut(amount0In, reserve0Before, reserve1Before);
+            emit AmountsIn(amount0In, amount1In);
+            emit AmountsOut(amount0Out, amount1Out);
+            (bool success1,) = user.proxy(address(testToken1), abi.encodeWithSelector(testToken1.transfer.selector, address(pair), amount0In));
+            require(success1);
+        } else {
+            require(amount1In <= balance1Before);
+            amount0In = 0;
+            amount1Out = 0;
+            amount0Out = UniswapV2Library.getAmountOut(amount1In, reserve1Before, reserve0Before);
+            emit AmountsIn(amount0In, amount1In);
+            emit AmountsOut(amount0Out, amount1Out);
+            (bool success1,) = user.proxy(address(testToken2), abi.encodeWithSelector(testToken2.transfer.selector, address(pair), amount1In));
+            require(success1);
+        }
 
         // Action:
+        (bool success2,) = user.proxy(address(pair), abi.encodeWithSelector(pair.swap.selector, amount0Out, amount1Out, address(user), ""));
 
         // Post-condition:
-
+        /* 1. Swap should be successful */
+        assert(success2);
+        /* 2. Reserves may change ,but k should be constant */
+        (uint reserve0After, uint reserve1After,) = pair.getReserves();
+        uint kAfter = reserve0After * reserve1After;
+        emit ReservesAfter(reserve0After, reserve1After);
+        assert(kBefore == kAfter);
+        /* 3. The change in the user's token balances should match our expectations */
+        uint balance0After = testToken1.balanceOf(address(user));
+        uint balance1After = testToken2.balanceOf(address(user));
+        emit BalancesAfter(balance0After, balance1After);
+        if (amount0In > amount1In) {
+            assert(balance0After == balance0Before - amount0In);
+            assert(balance1After == balance1After + amount1Out);
+        } else {
+            assert(balance1After == balance1Before - amount1In);
+            assert(balance0After == balance0Before + amount0Out);
+        }
     }
 
 }
